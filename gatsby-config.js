20453953b6caeb84fe0f534da27541cb1ab67d7b
cweave_ <cweav3r@gmail.com>
2022-01-31 23:03:09 -0500

    gatsby-plugin-gatsby-cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-plugin-sass`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-gatsby-cloud`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
